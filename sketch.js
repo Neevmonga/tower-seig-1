e70aa2360c0461d1a8e9678b27db421a8e869479
Neevmonga <72914539+Neevmonga@users.noreply.github.com>
2020-12-09 14:25:18 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,58 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint=Matter.Constraint
+var engine, world;
+var chain1;
+var chain2;
+var ground;
+var ball1;
+var ball2;
+var build1,build2,build3,build4,build5,build6,build7,build8,build9,build10
+
+
+
+function setup(){
+    var canvas = createCanvas(1400,500);
+
+    engine = Engine.create();
+    world = engine.world;
+     build1=new Box (500,20,70,70)
+     build2=new Box (570,20,70,70)
+     build3=new Box (640,20,70,70)
+     build4=new Box (710,20,70,70)
+     build5=new Box (780,20,70,70)
+     build6=new Box (570,20,70,70)
+     build7=new Box (640,20,70,70)
+     build8=new Box (710,20,70,70)
+     build9=new Box (640,20,70,70)
+    ground = new Ground(650,300,500,20);
+    
+    ball1=new ball(20,93,100,100,PI)
+    chain1= new rope(ball1.body,{x:150,y:80})
+
+    }
+
+function draw(){
+    background("aqua");
+    Engine.update(engine);
+     ball1.display();
+     chain1.display();
+
+    ground.display();
+    build1.display();
+    build2.display();
+    build3.display();
+    build4.display();
+    build5.display();
+    build6.display();
+    build7.display();
+    build8.display();
+    build9.display();
+    
+    
+    
+}
+function mouseDragged(){
+Matter.Body.setPosition(ball1.body,{x:mouseX,y:mouseY})
+}
